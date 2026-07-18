a0e1076b91dbf0e9cf90acafd00d7c9d3b209a36
elysia <3302843873@qq.com>
2026-07-18 20:37:46 +0800

    distro: jinx structure & mlibc package

---
+++ distro/build-systems/meson.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+meson_configure() {
+	meson setup \
+		. \
+		"${source_dir}" \
+		--cross-file="${base_dir}/toolchains/x86_64-roxy.cross-file" \
+		--prefix="${prefix}" \
+		--libdir=lib \
+		"$@"
+}
+
+meson_build() {
+	meson compile -C . -j "${parallelism}"
+}
+
+meson_install() {
+	DESTDIR="${dest_dir}" meson install -C . --no-rebuild
+}
